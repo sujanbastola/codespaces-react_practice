b34dfa209f1e1b3d445a810203602f817154df25
sujan bastola <49635653+sujanbastola@users.noreply.github.com>
2025-05-12 05:57:39 +0000

    practice 1 of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import React from 'react';
+import Navbar from "./application/navbar"; 
 function App() {
   return (
     <div className="App">
+       <Navbar />
         <p>
-          hello this is sujan its my first day on  React
+          hello this is sujan its my first day on  React. 
         </p>
     </div>
   );
 }
-
 export default App;
---
+++ src/application/navbar.jsx
@@ -0,0 +1,11 @@
+function navbar () {
+    return (
+        <div>
+            <li>
+                <a href="https://www.w3schools.com/REACT/react_components.asp">home </a>
+            </li>
+            <li>About</li>
+        </div>
+    )
+}
+ export default navbar
